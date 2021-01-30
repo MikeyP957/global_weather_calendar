51b9fd6d8cb087c6f1731d860d267e61076d5f01
Mikey P <peraram2@gmail.com>
2021-01-29 22:36:31 -0800

    linking js, css, jQuery, weather api, BS

---
+++ script/script.js
@@ -0,0 +1,3 @@
+const apiKey = "946fe7531174c49baf0d6b15b83dd09f";
+const inputVal = input.value;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
