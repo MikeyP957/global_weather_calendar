27a12a4dd715cadd6f47de7a7c2ca2f29ecfe938
Mikey P <peraram2@gmail.com>
2021-01-30 20:55:22 -0800

    start html outline

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
-const apiKey = "946fe7531174c49baf0d6b15b83dd09f";
-const inputVal = input.value;
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+// const apiKey = "946fe7531174c49baf0d6b15b83dd09f";
+// const inputVal = input.value;
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
