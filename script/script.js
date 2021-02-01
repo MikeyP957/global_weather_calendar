42dc65b5e4798d7d3c256b414369d9c4f9dcea60
Mikey P <peraram2@gmail.com>
2021-01-31 23:05:45 -0800

    5day forecast working

--- script/script.js
+++ script/script.js
@@ -6,6 +6,7 @@ var userSubmitBtn = document.querySelector(".button")
 var inputLocation = document.querySelector(".location")
 
 var weatherToday = document.querySelector(".todaysWeather");
+var weatherPredictions = document.querySelector(".five-day-display");
 var dayCard = document.querySelector(".day")
 
 
@@ -45,25 +46,27 @@ userSubmitBtn.addEventListener("click",function(){
 //a function that shows the weather for the future days
         .then(function(data){
             console.log(data, "future days data")
-            // for (i = 0; i < 4; i++){
-            //     //
-            // }
+            for (i = 1; i <= 5 ; i++){                
+                var tempData = data.list[i].main.temp;
+                var humidityData = data.list[i].main.humidity;
+                var windSpeedData = data.list[i].wind.speed;
+
+                var newCard = document.createElement("card")
+
+                newCard.innerHTML = "<p>Temperature: " + tempData + "</p> <br>" + 
+                "<p>Wind Speed: " + windSpeedData + "</p>" +
+                "<p>Humidity: " + humidityData + "</p> <br>" +
+                // "<p>UV Index: " + tempData + "</p> <br>"
+    
+                // cityAndDate.append(cityName + " " + dateAndTime)
+                weatherPredictions.appendChild(newCard)
+                console.log(tempData, "temp data for one day")
+            }
             
-            // var cityName = data.city.name;
-            // var dateAndTime = data.list[0].dt_txt;
-            // var tempData = data.list[0].main.temp;
-            // var humidityData = data.list[0].main.humidity;
-            // var windSpeedData = data.list[0].wind.speed;
+            
 
-            // var newDiv = document.createElement("div")
-            // newDiv.innerHTML = "<h3>" +cityName + " " + dateAndTime + "</h3>" +
-            // "<p>Temperature: " + tempData + "</p> <br>" + 
-            // "<p>Wind Speed: " + windSpeedData + "</p>" +
-            // "<p>Humidity: " + humidityData + "</p> <br>" +
-            // // "<p>UV Index: " + tempData + "</p> <br>"
 
-            // // cityAndDate.append(cityName + " " + dateAndTime)
-            // weatherToday.appendChild(newDiv)
+
         })
 
         
