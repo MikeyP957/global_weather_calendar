b5e4bee2e50b634ed6b9ab3107a0eff1948d0d9a
Mikey P <peraram2@gmail.com>
2021-02-02 18:28:38 -0800

    consolidating .then and working on weather icon

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ var userSubmitBtn = document.querySelector(".button")
 var inputLocation = document.querySelector(".location")
 
 var weatherToday = document.querySelector(".todaysWeather");
-console.log(weatherToday.value, "weatherToday Value");
+// console.log(weatherToday.value, "weatherToday Value");
 var weatherPredictions = document.querySelector(".five-day-display");
 var dayCard = document.querySelector(".day");
 
@@ -16,7 +16,7 @@ userSubmitBtn.addEventListener("click",function(){
     
     fetch("https://api.openweathermap.org/data/2.5/forecast?q="+ inputLocation.value +"&units=imperial&appid="+ apiKey)
         .then(function(response) {
-            return response.json();
+            return response.json();            
            })
            //create the div for today's weather
         //Create a function that gets all my objects/arrays and returns them as variables
@@ -38,48 +38,37 @@ userSubmitBtn.addEventListener("click",function(){
             };
                 console.log(weatherCoordinates.latitude, "latitude")
                 console.log(data, "this function returns weatherData")
-                return weatherCoordinates, data;
-        })
-        // fetch("http://api.openweathermap.org/data/2.5/uvi?lat=" + weatherCoordinates.latitude.value + "&lon=" + weatherCoordinates.longitude.value + "&appid="+ apiKey)
-        // .then(function(response) {
-        //     return response.json();
-        //    })
-        //    .then(function(data){
-        //
-        //    })
-        .then(function(data){
-            weatherData = {
+                                
+            var weatherData = {
                 cityName: data.city.name,
                 dateAndTime:data.list[2].dt_txt,
                 tempData:data.list[2].main.temp,
                 windSpeedData: data.list[2].wind.speed ,
                 humidityData:data.list[2].main.humidity,
+                weatherIcon: data.list[2].weather[0].icon,
             };
-            
-            
-           
-            
-        var newDiv = document.createElement("div");
-        newDiv.id = "weatherLocationNew"
-        newDiv.innerHTML = "<h3>" +weatherData.cityName + " " + weatherData.dateAndTime + "</h3>" +
-        "<p>Temperature: " + weatherData.tempData + " degrees Farenheight</p> <br>" + 
-        "<p>Wind Speed: " + weatherData.windSpeedData + " MPH</p>" +
-        "<p>Humidity: " + weatherData.humidityData + "%</p> <br>";
-        // "<p>UV Index: " + tempData + "</p> <br>"
-        // if(weatherToday.value == 0){
-        //     weatherToday.appendChild(newDiv)        
-        //     return data;
-        // }
-        // else{
-        //     weatherToday.replace(document.getElementById("weatherLocationNew"), newDiv)        
-        //     return data;
-        // }
-        weatherToday.appendChild(newDiv)
-        return data;
-        })
-//a function that shows the weather for the future days
-        .then(function(data){
-            console.log(data, "future days data")
+                        
+                
+            var newDiv = document.createElement("div");
+            newDiv.id = "weatherLocationNew"
+            newDiv.innerHTML = "<h3>" +weatherData.cityName + " " + weatherData.dateAndTime + "</h3>" +
+            "<div class='icon'><img src='"+ weatherData.weatherIcon +".png' alt='weather icon'></div>"
+            "<p>Temperature: " + weatherData.tempData + " degrees Farenheight</p> <br>" + 
+            "<p>Wind Speed: " + weatherData.windSpeedData + " MPH</p>" +
+            "<p>Humidity: " + weatherData.humidityData + "%</p> <br>";
+            // "<p>UV Index: " + tempData + "</p> <br>"
+            // if(weatherToday.value == 0){
+            //     weatherToday.appendChild(newDiv)        
+            //     return data;
+            // }
+            // else{
+            //     weatherToday.replace(document.getElementById("weatherLocationNew"), newDiv)        
+            //     return data;
+            // }
+            weatherToday.appendChild(newDiv)
+
+
+
             for (i = 1; i <= 5 ; i++){                
                 var tempData = data.list[(8*i-1)].main.temp;
                 var humidityData = data.list[(8*i-1)].main.humidity;
@@ -98,17 +87,17 @@ userSubmitBtn.addEventListener("click",function(){
                 // cityAndDate.append(cityName + " " + dateAndTime)
                 weatherPredictions.appendChild(newCard)
                 console.log(tempData, "temp data for one day")
-            }       
-
+            }
 
 
         })
-
-
-        
+           
         .catch(function(err){
             console.log("fetch problem: " + err.message)
            
         })
  
 } )
+
+
+// fetch("http://api.openweathermap.org/data/2.5/uvi?lat=" + weatherCoordinates.latitude + "&lon=" + weatherCoordinates.longitude + "&appid="+ apiKey)
