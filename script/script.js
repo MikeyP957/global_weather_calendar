bf2c9d85fa3f4e59d8a55874935540b24204185a
Mikey P <peraram2@gmail.com>
2021-02-03 22:13:35 -0800

    refractor

--- script/script.js
+++ script/script.js
@@ -1,15 +1,14 @@
  const apiKey = "946fe7531174c49baf0d6b15b83dd09f";
-// const inputVal = input.value;
-// const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
+//user input and submit from html
 var userSubmitBtn = document.querySelector(".button")
 var inputLocation = document.querySelector(".location")
-// get from dop down in html and update in fetch var stateID = ,
 
+//where todays data will go
 var weatherToday = document.querySelector(".todaysWeather");
-// console.log(weatherToday.value, "weatherToday Value");
+// where the five day forecast will go
 var weatherPredictions = document.querySelector(".five-day-display");
-var dayCard = document.querySelector(".day");
+
 
 
 userSubmitBtn.addEventListener("click",function(){
@@ -19,17 +18,6 @@ userSubmitBtn.addEventListener("click",function(){
         .then(function(response) {
             return response.json();            
            })
-           //create the div for today's weather
-        //Create a function that gets all my objects/arrays and returns them as variables
-        //function that makes the main weather for that day
-        //function that makes weather prediction for 5 days
-            //append as cards
-            //use a for loop, the array would be index [8i-1], unless i = 0
-        //function that keeps local storage and appends as list
-        // .then(function(){
-        //     if 
-        // })
-
         .then(function(data){
             console.log(data);
             
@@ -40,7 +28,7 @@ userSubmitBtn.addEventListener("click",function(){
                 // console.log(weatherCoordinates.latitude, "latitude")
                 console.log(data, "this function returns weatherData")
 
-
+// storing the data to append
             var weatherData = {
                 cityName: data.city.name,
                 dateAndTime:data.list[2].dt_txt,
@@ -49,6 +37,7 @@ userSubmitBtn.addEventListener("click",function(){
                 humidityData:data.list[2].main.humidity,
                 weatherIcon: data.list[2].weather[0].icon,
             };
+           
                         
                 
             var newDiv = document.createElement("div");
@@ -56,44 +45,43 @@ userSubmitBtn.addEventListener("click",function(){
             newDiv.id = "weatherLocationNew"
             newDiv.innerHTML = "<h3>" +weatherData.cityName + " " + weatherData.dateAndTime + "</h3>" +
             "<div class='icon'><img src='http://openweathermap.org/img/wn/"+ weatherData.weatherIcon + "@2x.png' alt='weather icon'></div><p>Temperature: " + weatherData.tempData + " degrees Farenheight</p> <br>" + "<p>Wind Speed: " + weatherData.windSpeedData + " MPH</p>" +
-            "<p>Humidity: " + weatherData.humidityData + "%</p> <br>";
-           // "<p>UV Index: " + tempData + "</p> <br>"
-            // if(weatherToday.value == 0){
-            //     weatherToday.appendChild(newDiv)        
-            //     return data;
-            // }
-            // else{
-            //     weatherToday.replace(document.getElementById("weatherLocationNew"), newDiv)        
-            //     return data;
-            // }
+            "<p>Humidity: " + weatherData.humidityData + "%</p> <br>"
+            
             weatherToday.appendChild(newDiv)
 
 
-
+// the data is recursive, each 8th index is a new day. There is no need for the initial index as it is already displayed
             for (i = 1; i <= 5 ; i++){                
                 var tempData = data.list[(8*i-1)].main.temp;
                 var humidityData = data.list[(8*i-1)].main.humidity;
                 var windSpeedData = data.list[(8*i-1)].wind.speed;
-                var weatherIcon = data.list[(8*i-1)].weather[0].icon
+                var weatherIcon = data.list[(8*i-1)].weather[0].icon;
+                var weatherDate = data.list[(8*i-1)].dt_txt;
+                
 
                
                 var newCard = document.createElement("div");
                 newCard.classList.add("card");
                 newCard.classList.add("col-sm-2")
 
-                newCard.innerHTML = "<p>Temperature: " + tempData + " deg. F</p> <br>" + "<div class='icon'><img src='http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png' alt='weather icon'></div><p>Wind Speed: " + windSpeedData + " MPH</p>" +
-                "<p>Humidity: " + humidityData + "%</p> <br>" //+
-                // "<p>UV Index: " + tempData + "</p> <br>"
+                newCard.innerHTML = "<h5>" + weatherDate + "</h5><br><p>Temperature: " + tempData + " deg. F</p> <br>" + "<div class='icon'><img src='http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png' alt='weather icon'></div><p>Wind Speed: " + windSpeedData + " MPH</p>" +
+                "<p>Humidity: " + humidityData + "%</p> <br>"
     
                 // cityAndDate.append(cityName + " " + dateAndTime)
                 weatherPredictions.appendChild(newCard)
                 // console.log(tempData, "temp data for one day")
             }
-
-            fetch("http://api.openweathermap.org/data/2.5/uvi?lat=" + weatherCoordinates.latitude + "&lon=" + weatherCoordinates.longitude + "&appid="+ apiKey)
+// fetch uvi data using the coordinates from the first fetch
+            fetch("http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + weatherCoordinates.latitude + "&lon=" + weatherCoordinates.longitude + "&appid="+ apiKey)
             .then(function(response){
-                console.log(response, "this is the uvi fetch")
-            })   
+                return(response.json())
+            })
+            .then(function(data2){
+                console.log(data2, "this is uvi data");
+                
+             
+            })
+            
         })
           
 
